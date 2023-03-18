d5f4d42a5ecfa9ac01a19f2a491aab432e8ec174
DainisKudrjavcevs <125664089+DainisKudrjavcevs@users.noreply.github.com>
2023-03-18 19:28:19 +0000

    main

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import heapq
 
 def parallel_processing(n, m, data):
     output = []
-    threads = [(0, i) for i in range(n)]
-    heapq.heapify(threads)
+    pavedieni = [(0, i) for i in range(n)]
+    heapq.heapify(pavedieni)
     for i in range(m):
         time = data[i]
-        f_time, thread_idx = heapq.heappop(threads)
+        f_time, pavediens = heapq.heappop(pavedieni)
         if output:
             s_time = max(f_time, output[-1][1])
         else:
             s_time = f_time
         f_time = s_time + time
-        output.append((thread_idx, s_time))
-        heapq.heappush(threads, (f_time, thread_idx))
+        output.append((pavediens, s_time))
+        heapq.heappush(pavedieni, (f_time, pavediens))
     return output
 
 
@@ -23,8 +23,8 @@ def main():
 
     result = parallel_processing(n, m, data)
 
-    for thread_idx, start_time in result:
-        print(thread_idx, start_time)
+    for pavediens, start_time in result:
+        print(pavediens, start_time)
 
 
 if __name__ == "__main__":
