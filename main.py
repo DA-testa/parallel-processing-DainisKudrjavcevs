935242828cdc9757e1ffc8e2abc97a0ed7c0f576
DainisKudrjavcevs <125664089+DainisKudrjavcevs@users.noreply.github.com>
2023-03-18 19:07:06 +0000

    main

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import heapq
 
 def parallel_processing(n, m, data):
     output = []
-    threads  = [(0,i) for i in range(n)]
-    heapq.heapify(threads )
-
+    threads = [(0, i) for i in range(n)]
+    heapq.heapify(threads)
     for i in range(m):
         time = data[i]
         f_time, thread_idx = heapq.heappop(threads)
         s_time = max(f_time, output[-1][1] if output else 0)
-        f_time = s_time = time
+        f_time = s_time + time
         output.append((thread_idx, s_time))
         heapq.heappush(threads, (f_time, thread_idx))
-        
     return output
 
 def main():
@@ -27,3 +25,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
